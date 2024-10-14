bf0e16e71aded0f5ff6bb4d975a8b0eff35c2aa6
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-10-14 17:06:08 +0400

    Bug fix for direct links

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
-        if (changeInfo.url) {
-            chrome.tabs.sendMessage( tabId, {
-                message: 'ytvideoid',
-                id: youtube_parser(changeInfo.url),
-            })
-        }
+      if (changeInfo.url && id = youtube_parser(changeInfo.url)) {
+        chrome.tabs.sendMessage( tabId, {
+          message: 'ytvideoid',
+          id: id
+        })
+      }
     }
-)
-function youtube_parser(url) {
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
-    var match = url.match(regExp)
-    return (match && match[7].length == 11) ? match[7] : false
-}
+  );
+  function youtube_parser(url) {
+      var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+      var match = url.match(regExp);
+      return (match && match[7].length == 11) ? match[7] : false;
+  }
